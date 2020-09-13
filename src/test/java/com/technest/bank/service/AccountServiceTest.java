e459a49ff25af2591a712867bac450d339b9c15f
Marc <marc.pedemonte@gmail.com>
2020-09-13 17:03:21 +0200

    add findById method tests

--- src/test/java/com/technest/bank/service/AccountServiceTest.java
+++ src/test/java/com/technest/bank/service/AccountServiceTest.java
@@ -2,6 +2,7 @@ package com.technest.bank.service;
 
 import com.technest.bank.dao.AccountRepository;
 import com.technest.bank.dto.AccountDto;
+import com.technest.bank.exception.AccountNotFoundException;
 import com.technest.bank.model.Account;
 import com.technest.bank.service.impl.AccountServiceImpl;
 import com.technest.bank.service.impl.MapperImpl;
@@ -55,5 +56,44 @@ public class AccountServiceTest {
     ResponseEntity<List<AccountDto>> result = accountService.findAll();
     Assert.assertNotNull(result);
     Assert.assertEquals(result, ResponseEntity.ok().body(accountsDto));
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void findByIdWithNullArgumentTest() throws AccountNotFoundException {
+    // Given
+    final Integer id = null;
+
+    // When
+
+    // Then
+    accountService.findById(id);
+  }
+
+  @Test(expected = AccountNotFoundException.class)
+  public void findByIdWithNonExistingIdTest() throws AccountNotFoundException {
+    // Given
+    final Integer id = 1;
+
+    // When
+
+    // Then
+    accountService.findById(id);
+  }
+
+  @Test
+  public void findByIdWithExistingIdTest() throws AccountNotFoundException {
+    // Given
+    final Integer id = 1;
+    Account account = new Account(1, "Account1", Money.of(50, "EUR"), false);
+    AccountDto accountDto = new AccountDto(1, "Account1", Monetary.getCurrency("EUR"),
+        Money.of(50, "EUR"), false);
+
+    // When
+    Mockito.when(accountRepository.findById(id)).thenReturn(java.util.Optional.of(account));
+
+    // Then
+    ResponseEntity<AccountDto> result = accountService.findById(id);
+    Assert.assertNotNull(result);
+    Assert.assertEquals(result, ResponseEntity.ok().body(accountDto));
   }
 }
