06757a7eb1559ab759b31b86871b8fbd363cdb1f
Marc <marc.pedemonte@gmail.com>
2020-09-13 16:31:54 +0200

    Create TreasuryModifiedException

---
+++ src/main/java/com/technest/bank/exception/TreasuryModifiedException.java
@@ -0,0 +1,8 @@
+package com.technest.bank.exception;
+
+public class TreasuryModifiedException extends Exception {
+
+  public TreasuryModifiedException(String message) {
+    super(message);
+  }
+}
