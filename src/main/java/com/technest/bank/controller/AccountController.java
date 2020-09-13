11ba6ef164c7381fc47ac9efefc2ffada51c9899
Marc <marc.pedemonte@gmail.com>
2020-09-13 16:04:39 +0200

    add GET methods

--- src/main/java/com/technest/bank/controller/AccountController.java
+++ src/main/java/com/technest/bank/controller/AccountController.java
@@ -1,5 +1,33 @@
 package com.technest.bank.controller;
 
+import com.technest.bank.dto.AccountDto;
+import com.technest.bank.service.AccountService;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/account")
 public class AccountController {
 
+  @Autowired
+  private AccountService accountService;
+
+  @GetMapping
+  @ResponseStatus(HttpStatus.OK)
+  public ResponseEntity<List<AccountDto>> findAll() {
+    return accountService.findAll();
+  }
+
+  @GetMapping("/{id}")
+  @ResponseStatus(HttpStatus.OK)
+  public ResponseEntity<AccountDto> findById(@PathVariable("id") Integer id) {
+    return accountService.findById(id);
+  }
 }
--- src/main/java/com/technest/bank/service/AccountService.java
+++ src/main/java/com/technest/bank/service/AccountService.java
@@ -1,5 +1,12 @@
 package com.technest.bank.service;
 
+import com.technest.bank.dto.AccountDto;
+import java.util.List;
+import org.springframework.http.ResponseEntity;
+
 public interface AccountService {
 
+  ResponseEntity<List<AccountDto>> findAll();
+
+  ResponseEntity<AccountDto> findById(Integer id);
 }
