f0ac0aea881a51f48744ab5c7931983071d795d1
Marc <marc.pedemonte@gmail.com>
2020-09-13 16:32:19 +0200

    Define mapper methods

--- src/main/java/com/technest/bank/service/Mapper.java
+++ src/main/java/com/technest/bank/service/Mapper.java
@@ -1,5 +1,12 @@
 package com.technest.bank.service;
 
+import com.technest.bank.dto.AccountDto;
+import com.technest.bank.dto.AccountPostDto;
+import com.technest.bank.model.Account;
+
 public interface Mapper {
 
+  AccountDto mapToDto(Account account);
+
+  Account mapToEntity(AccountPostDto accountPostDto);
 }
