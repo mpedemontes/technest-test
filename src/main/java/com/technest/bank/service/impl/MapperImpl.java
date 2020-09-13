2b34fe6ade34dc9ec02bd4589df6408bb802af30
Marc <marc.pedemonte@gmail.com>
2020-09-13 16:35:57 +0200

    implement mapper methods

--- src/main/java/com/technest/bank/service/impl/MapperImpl.java
+++ src/main/java/com/technest/bank/service/impl/MapperImpl.java
@@ -1,5 +1,37 @@
 package com.technest.bank.service.impl;
 
-public class MapperImpl {
+import com.technest.bank.dto.AccountDto;
+import com.technest.bank.dto.AccountPostDto;
+import com.technest.bank.model.Account;
+import com.technest.bank.service.Mapper;
+import javax.money.CurrencyUnit;
+import javax.money.Monetary;
+import org.javamoney.moneta.Money;
+import org.springframework.stereotype.Service;
 
+@Service
+public class MapperImpl implements Mapper {
+
+  @Override
+  public AccountDto mapToDto(Account account) {
+    return new AccountDto(
+        account.getId(),
+        account.getName(),
+        account.getBalance().getCurrency(),
+        account.getBalance(),
+        account.getTreasury()
+    );
+  }
+
+  @Override
+  public Account mapToEntity(AccountPostDto accountPostDto) {
+    CurrencyUnit currencyUnit = Monetary.getCurrency(accountPostDto.getCurrency());
+    Money money = Money.of(accountPostDto.getBalance(), currencyUnit);
+    return new Account(
+        null,
+        accountPostDto.getName(),
+        money,
+        accountPostDto.getTreasury()
+    );
+  }
 }
