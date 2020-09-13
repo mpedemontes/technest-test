3c8be13ab0f4b78ff0b543cd28ae709bee87b049
Marc <marc.pedemonte@gmail.com>
2020-09-13 16:13:05 +0200

    DTOs definition

--- src/main/java/com/technest/bank/dto/AccountDto.java
+++ src/main/java/com/technest/bank/dto/AccountDto.java
@@ -1,5 +1,19 @@
 package com.technest.bank.dto;
 
+import javax.money.CurrencyUnit;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.javamoney.moneta.Money;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class AccountDto {
 
+  private Integer id;
+  private String name;
+  private CurrencyUnit currency;
+  private Money balance;
+  private Boolean treasury;
 }
--- src/main/java/com/technest/bank/dto/AccountPostDto.java
+++ src/main/java/com/technest/bank/dto/AccountPostDto.java
@@ -1,5 +1,17 @@
 package com.technest.bank.dto;
 
+import java.math.BigDecimal;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class AccountPostDto {
 
+  private String name;
+  private String currency;
+  private BigDecimal balance;
+  private Boolean treasury;
 }
