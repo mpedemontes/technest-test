6927c3da072aca012248851e59f45d29f90f464a
Marc <marc.pedemonte@gmail.com>
2020-09-13 16:06:01 +0200

    add POST method

--- src/main/java/com/technest/bank/controller/AccountController.java
+++ src/main/java/com/technest/bank/controller/AccountController.java
@@ -1,6 +1,7 @@
 package com.technest.bank.controller;
 
 import com.technest.bank.dto.AccountDto;
+import com.technest.bank.dto.AccountPostDto;
 import com.technest.bank.service.AccountService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +9,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
@@ -29,5 +32,11 @@ public class AccountController {
   @ResponseStatus(HttpStatus.OK)
   public ResponseEntity<AccountDto> findById(@PathVariable("id") Integer id) {
     return accountService.findById(id);
+  }
+
+  @PostMapping
+  @ResponseStatus(HttpStatus.CREATED)
+  public ResponseEntity<AccountDto> addAccount(@RequestBody AccountPostDto accountPostDto) {
+    return accountService.addAccount(accountPostDto);
   }
 }
--- src/main/java/com/technest/bank/service/AccountService.java
+++ src/main/java/com/technest/bank/service/AccountService.java
@@ -1,6 +1,7 @@
 package com.technest.bank.service;
 
 import com.technest.bank.dto.AccountDto;
+import com.technest.bank.dto.AccountPostDto;
 import java.util.List;
 import org.springframework.http.ResponseEntity;
 
@@ -9,4 +10,6 @@ public interface AccountService {
   ResponseEntity<List<AccountDto>> findAll();
 
   ResponseEntity<AccountDto> findById(Integer id);
+
+  ResponseEntity<AccountDto> addAccount(AccountPostDto accountPostDto);
 }
