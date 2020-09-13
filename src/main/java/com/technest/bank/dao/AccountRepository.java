1ae7a465032ea1576e693b02c4068952ebb7f48d
Marc <marc.pedemonte@gmail.com>
2020-09-13 16:21:10 +0200

    make AccountRepository extend JpaRepository

--- src/main/java/com/technest/bank/dao/AccountRepository.java
+++ src/main/java/com/technest/bank/dao/AccountRepository.java
@@ -1,5 +1,10 @@
 package com.technest.bank.dao;
 
-public interface AccountRepository {
+import com.technest.bank.model.Account;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AccountRepository extends JpaRepository<Account, Integer> {
 
 }
